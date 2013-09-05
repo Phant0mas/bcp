36046f06db2a77385e303fe2fbf6afa97553e7a9
Manolis Ragkoussis <manolis837@gmail.com>
2013-09-05 19:11:12 +0300

    Added a license and starting working on issue 4 on the bcp project from jwilberding @ https://github.com/jwilberding/bcp

---
+++ src/md5hash.h
@@ -0,0 +1,7 @@
+/**
+*	This file is written from Manolis 'phant0mas' Ragkousis 
+*	as part of the bcp project from Jordan Wilberding aka jwilberding
+*
+* 	This file is distributed under the terms of the GNU/GPL v3 license.
+* 	See the file COPYING in the root folder of the project for more information.*
+**/
