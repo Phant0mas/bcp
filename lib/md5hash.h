3cacf5fb82d9afd9da08ca96516979837936d4b5
Manolis Ragkoussis <manolis837@gmail.com>
2013-09-06 16:32:04 +0300

    Declaration of function md5generate() at md5hash.h

--- lib/md5hash.h
+++ lib/md5hash.h
@@ -4,6 +4,7 @@
 * 	This file is distributed under the terms of the GNU/GPL v3 license.
 * 	See the file LICENSE folder in the root folder of the project for more information.
 **/
+
 #ifndef MD5HASH_H_INCLUDED
 #define MD5HASH_H_INCLUDED
 
@@ -12,6 +13,8 @@
 #include <openssl/md5.h>
 //we use the openssl md5 library for our purpose 
 
+//generates a md5 hash for a file,returns a character array with that hash
+char *md5generate (char *filename) ;
 
 
 #endif
